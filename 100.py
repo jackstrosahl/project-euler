0486d60e76b951be7544ddb140dd4ce0989e4e76
jackstrosahl <strosahljack@gmail.com>
2021-12-27 18:07:02 -0500

    Remove loop stuff

--- 100.py
+++ 100.py
@@ -5,15 +5,10 @@ min_total = 1e12+1
 blue_amt = sqrt(.5)
 
 for total in range(int(min_total), int(1e13)):
-    min_blue = ceil(total*blue_amt)
-    done=False
-    for blue in range(min_blue, min_blue+10):
-        chance = Fraction(blue,total)*Fraction(blue-1,total-1)
-        if chance == half:
-            print(f"Blue: {blue}, Red: {total-blue}, Total: {total}, Chance: {chance}")
-            done=True
-            break
+    blue = ceil(total*blue_amt)
+    chance = Fraction(blue,total)*Fraction(blue-1,total-1)
+    if chance == half:
+        print(f"Blue: {blue}, Red: {total-blue}, Total: {total}, Chance: {chance}")
+        break
     if total % 1000000 == 0:
         print(f"Still going, Total: {total}")
-    if done:
-        break

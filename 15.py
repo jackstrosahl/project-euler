9dd8a203722d63d6ce088cf294c13211afbd82b2
jackstrosahl <strosahljack@gmail.com>
2021-12-29 16:24:19 -0500

    15 done, 95 WIP

--- 15.py
+++ 15.py
@@ -1,15 +1,2 @@
-w = 20
-h = 20
-def count_solutions(pos=(0,0)):
-    print(pos)
-    num_solutions = 0
-    x,y = pos
-    if x+1 <= w:
-        num_solutions += count_solutions((x+1,y))
-    if y+1 <= h:
-        num_solutions += count_solutions((x,y+1))
-    if x == w and y == h:
-        num_solutions += 1
-    return num_solutions
-
-print(count_solutions())
+from math import factorial
+print(factorial(40)/factorial(20)**2)
--- 95.py
+++ 95.py
@@ -1,1 +1,16 @@
-for i in range
+from math import sqrt, floor
+
+for i in range(1,int(1e6+1)):
+    num = i
+    while True:
+        div_sum = 0
+        for div in range(1,floor(sqrt(num))+1):
+            if num % div == 0:
+                div_sum += div
+                if div != 1:
+                    div_sum += num/div
+        if num == div_sum:
+            break
+        num = div_sum
+        if div_sum == i:
+            break

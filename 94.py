86f9127f5cbe69106a2651cf2d360f193dfb164d
strojac <strosahljack@gmail.com>
2022-10-21 14:06:48 -0700

    94 done

--- 94.py
+++ 94.py
@@ -1,25 +1,26 @@
-from math import ceil, sqrt
+from math import ceil
 from decimal import Decimal
+from multiprocessing import Pool
 
 max_perim = 1e9
 
-def get_area(a,b,c):
-    s = (a+b+c)/2
-    return sqrt(s*(s-a)*(s-b)*(s-c))
-def is_valid(a,b,c):
-    return not (a < 0 or b < 0 or c < 0 or (a+b <= c) or (a+c <=b) or (b+c <=a))
+def get_area(a,b):
+    return (b/4)*((4*a**2-b**2).sqrt())
 
-total = 0
-
-for ab in range(2,ceil(max_perim/3)):
+def check_area(ab):
+    ans = 0
     if ab % 10e6 == 0:
         print(f"Still going.  A=B={ab}")
     for c in (ab-1,ab+1):
         perim = ab+ab + c
         if perim > max_perim:
            continue
-        area = get_area(Decimal(ab),Decimal(ab),Decimal(c))
-        if area.is_integer():
-            total += perim
+        area = get_area(Decimal(ab),Decimal(c))
+        if area % 1 == 0:
+            ans += perim
+    return ans
 
-print(total)
+with Pool() as pool:
+    res = pool.map(check_area, range(2,ceil(max_perim/3)+10), chunksize=10000)
+
+print(sum(res))

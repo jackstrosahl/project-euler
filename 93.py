d57cfab889e251d96aadcc8e6db8e16f8dce3035
strojac <strosahljack@gmail.com>
2022-10-22 11:16:18 -0700

    93 Done

--- 93.py
+++ 93.py
@@ -3,43 +3,42 @@ from operator import add, sub, mul, truediv as div
 
 ops = (add,sub,mul,div)
 
-
-"""
-(2*(3-4))*5 = -10 
-2*((3-4)*5) = -10 
-"""
-def get_targets(digits,ops):
-    simple = ops[0](digits[0],digits[1])
-    if len(digits) == 2:
-        assert(len(ops) == 1)
-        return [simple]
-    assert len(digits) > 2
+def get_targets(digits,ops,lhs=None):
+    if len(digits) == 0:
+        return [lhs]
+    rhs = digits[0]
+    digits = digits[1:]
+    if lhs==None:
+        return get_targets(digits,ops,rhs)
+    op = ops[0]
+    ops = ops[1:]
     out = []
-    for rhs in get_targets(digits[1:],ops[1:]):
+    for orhs in get_targets(digits,ops,rhs):
         try:
-            out.append(ops[0](digits[0],rhs))
+            out.append(op(lhs,orhs))
         except ZeroDivisionError:
             pass
-    for lhs in get_targets(digits[:-1],ops[:-1]):
+    if len(ops) > 0: 
         try:
-            out.append(ops[-1](lhs,digits[-1]))
+            out.extend(get_targets(digits,ops,op(lhs,rhs)))
         except ZeroDivisionError:
             pass
-    try:
-        out.extend(get_targets([simple,*digits[2:]],ops[1:]))
-    except ZeroDivisionError:
-        pass
     return out
     
 
+best = 0
+best_digits = None
 for digits in combinations(range(1,10),4):
-    pass
-
-for digits in ((1,2,3,4),):
     targets = set()
     for digorder in permutations(digits,4):
         for oporder in product(ops,repeat=3):
-            targets.update(filter(lambda x: x%1 ==0 and x > 0, get_targets(digorder,oporder)))
-    print(len(targets), sorted(targets))
+            targets.update(filter(lambda x: x % 1 == 0 and x > 0, get_targets(digorder,oporder)))
+    
+    expressible = 0
+    while expressible+1 in targets:
+        expressible+=1
+    if expressible > best:
+        best = expressible
+        best_digits = digits
 
-print(get_targets((2,3,4,5),(mul,sub,mul)))
+print(best, best_digits)

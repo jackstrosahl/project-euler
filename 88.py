5c99c03b5ff8e547639c7590dac5fe4a1a33a3bc
strojac <strosahljack@gmail.com>
2022-10-23 11:06:42 -0700

    88 Wrong Approach

--- 88.py
+++ 88.py
@@ -11,22 +11,30 @@ class HCounter(Counter):
         return self.__key() == other.__key()
 
 @lru_cache(maxsize=int(1e4))
-def get_ps_set(nums):
+def get_ps_set(nums,tsum,tprod):
     cur_sum = counter_sum(nums)
     cur_prod = counter_product(nums)
-    if cur_sum == cur_prod:
+    if cur_sum == tsum and cur_prod == tprod:
         return nums
-    else:
-        assert cur_prod < cur_sum, nums
-        for val in sorted(nums):
-            new_nums = HCounter(nums)
-            new_nums[val] -= 1
-            new_nums[val+1] += 1
-            if counter_product(new_nums) <= counter_sum(new_nums):
-                res = get_ps_set(new_nums)
-                if res:
-                    return res
-        return None
+
+    assert cur_sum < tsum or cur_prod < tprod, (nums,tsum,tprod)
+    for val in sorted(nums):
+        new_nums = HCounter(nums)
+        assert new_nums[val] > 0
+        new_nums[val] -= 1
+        if new_nums[val] == 0:
+            del new_nums[val]
+        if len(new_nums) > 0 and tprod % val == 0 and counter_sum(new_nums) <= tsum and counter_product(new_nums) <= tprod:
+            res = get_ps_set(new_nums,tsum-val,tprod/val)
+            if res:
+                res[val] += 1
+                return res
+        new_nums[val+1] += 1
+        if counter_sum(new_nums) <= tsum and counter_product(new_nums) <= tprod:
+            res = get_ps_set(new_nums,tsum,tprod)
+            if res:
+                return res
+    return None
 
 def counter_product(nums):
     return reduce(lambda a,b:a*b, (num**count for num, count in nums.items()))
@@ -35,12 +43,15 @@ def counter_sum(nums):
     return sum(num*count for num, count in nums.items())
 
 def get_psn(k):
-    if k % 100 == 0:
-        print(f"Still going, k={k}")
-    nums = get_ps_set(HCounter({1:k}))
+    print(f"Still going, k={k}")
+    n = k
+    nums = None
+    while nums is None:
+        nums = get_ps_set(HCounter({1:k}),n,n)
+        n+=1
     return counter_sum(nums)
 
 with Pool() as pool:
-    res = pool.map(get_psn, range(2,12001),chunksize=100)
+    res = map(get_psn, range(2,12001))
 
 print(sum(set(res)))

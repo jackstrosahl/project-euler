7bea39f33c12d81fd87de8d0d1058769a151cd39
strojac <strosahljack@gmail.com>
2022-10-28 14:45:48 -0700

    88 Wrong

--- 88.py
+++ 88.py
@@ -1,5 +1,7 @@
+import chunk
 from collections import Counter
 from functools import lru_cache, reduce
+from math import inf
 from multiprocessing import Pool
 
 class HCounter(Counter):
@@ -10,31 +12,37 @@ class HCounter(Counter):
     def __eq__(self, other):
         return self.__key() == other.__key()
 
-nums_cache = {}
 @lru_cache(maxsize=int(1e4))
 def get_ps_set(nums):
-    k = nums.total()
+    print(nums)
     cur_sum = counter_sum(nums)
     cur_prod = counter_product(nums)
-    nums_cache[(k,cur_sum,cur_prod)] = nums
     if cur_sum == cur_prod:
         return nums
     else:
         assert cur_prod < cur_sum, nums
-        cache_nums = HCounter(nums)
-        cache_k = k
         for val in sorted(nums):
-            for to_remove in range(1,nums[val]+1):
-                cache_k -= 1
-                cache_nums[val] -= 1
-                res = nums_cache.get((cache_k))
+            assert nums[val] >= 0
             new_nums = HCounter(nums)
-            new_nums[val] -= 1
-            new_nums[val+1] += 1
-            if counter_product(new_nums) <= counter_sum(new_nums):
+            def increment(val):
+                new_nums[val] -= 1
+                if new_nums[val] == 0:
+                    del new_nums[val]
+                new_nums[val+1] += 1
+                
+            increment(val)
+            val += 1
+            diff = counter_sum(new_nums) - counter_product(new_nums)
+            last_diff = inf
+            while diff >= 0 and diff < last_diff:
                 res = get_ps_set(new_nums)
                 if res:
                     return res
+                increment(val)
+                val += 1
+                last_diff = diff
+                diff = counter_sum(new_nums) - counter_product(new_nums)
+                
         return None
 
 def counter_product(nums):
@@ -44,11 +52,12 @@ def counter_sum(nums):
     return sum(num*count for num, count in nums.items())
 
 def get_psn(k):
-    if k % 100 == 0:
+    if k % 10 == 0:
         print(f"Still going, k={k}")
     nums = get_ps_set(HCounter({1:k}))
     return counter_sum(nums)
 
-res = map(get_psn, range(2,12001))
+with Pool() as pool:
+    res = map(get_psn, range(2,12001))
 
 print(sum(set(res)))
